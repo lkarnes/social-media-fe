a8e0293de667013ac83c9cc965440138d823e50f
Logan Karnes <logan.karnes98@gmail.com>
2020-10-22 23:14:40 -0700

    started building comment drop down component

---
+++ src/components/subComponents/Comments.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import connect from 'react-redux';
+
+function Comments(props){
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default connect()(Comments)
